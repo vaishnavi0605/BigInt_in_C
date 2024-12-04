f7a05e5f5cc41ee42338a208a1a1f741d2887a45
Vaishnavi Pal <148760370+vaishnavi0605@users.noreply.github.com>
2024-12-04 14:39:18 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,3 @@
-/*    
- CS201- DSA Project 
- Project Title : BIGINT & Calculation of PI
- Instructor : Dr. Anil Shukla
- Mentor : Avadhesh Gaur
-
- Anant Prakash Singh -2021CSB1066  
- Kartik Yadav -2021CSB1101  
- Aditya Kumar Sahu -2021MCB1228
-
-*/
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
